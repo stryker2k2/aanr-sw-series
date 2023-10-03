29112208bd78865bf5be6078677a2a698dd5cd85
Jack Ward <stryker2k2@msn.com>
2023-10-03 12:40:59 -0500

    Added Race Placeholder Cards

--- support/push.sh
+++ support/push.sh
@@ -2,15 +2,27 @@
 
 # Push website to Linode Server for hosting
 # Ensure that Permissions match (ex: /static/images must be 'chown 666')
+# Launch from 'aanr-sw-series' Root Directory
+
+RED="\e[31m"
+GREEN="\e[32m"
+ENDCOLOR="\e[0m"
 
 # Linode Server IP
 SERVER='66.228.48.250'
 
 # Ensure Server is Available
-ping ${SERVER} -c 4
+echo -e "${GREEN}[+] Ensuring Server is Online${ENDCOLOR}"
+ping ${SERVER} -c 1
 
 # Launch Server-side Script to Remove ALL Website Files
+echo -e "${GREEN}[+] Launching Server-side Script to Remove ALL Website Files${ENDCOLOR}"
 ssh root@${SERVER} 'bash -c /root/clean_server.sh'
 
 # Copy Local Website Files to Server
-scp -rp ../static ../support ../templates ../app.py ../app.wsgi ../flaskapp.conf ../webforms.py root@${SERVER}:/var/www/aanr-sw-series/
+echo -e "${GREEN}[+] Copying Local Website Files to Server${ENDCOLOR}"
+scp -rp static support templates app.py app.wsgi flaskapp.conf webforms.py root@${SERVER}:/var/www/aanr-sw-series/
+
+# Send the Command to Restart the Server
+echo -e "${GREEN}[+] Sending the Command to Restart the Server${ENDCOLOR}"
+ssh root@${SERVER} 'bash -c /root/restart_apache2.sh'
